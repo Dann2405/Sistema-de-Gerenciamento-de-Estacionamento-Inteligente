d7fa97080c967b67991992e2ca361d28c11fbff5
Dann2405 <danielsoares0558@gmail.com>
2025-05-04 14:50:58 -0300

    refactor: veiculo.h

--- src/include/veiculo.h
+++ src/include/veiculo.h
@@ -6,48 +6,82 @@
 
 using namespace std;
 
-class Veiculo {
-    private:
-        string placa; // Placa do veículo
-        string modelo; // Modelo do veículo
-        string tipo; // Comum, Prioritário ou de Emergência
-    public:
-        Veiculo()
+class Veiculo
+{
+private:
+    string placa; // Placa do veículo
+    string tipo;  // Comum, Prioritário ou de Emergência
+public:
+    Veiculo()
+    {
+        placa = "";
+        tipo = "";
+    }
+    ~Veiculo()
+    {
+    }
+    void set_dados()
+    {
+        cout << "\n--- INSERIR UM NOVO VEICULO ---\n";
+        cout << "Qual a placa do veiculo? ";
+        getline(cin >> ws, placa);
+
+        cout << "Informe o tipo de veiculo:\n";
+        cout << "1. Comum\n";
+        cout << "2. Prioritário\n";
+        cout << "3. Emergência\n";
+        cout << "4. Serviço\n";
+        cout << "Escolha uma opção: ";
+
+        int opcao;
+        cin >> opcao;
+        cin.ignore(); // Limpa o buffer de entrada
+
+        switch (opcao)
         {
-            placa = "";
-            modelo = "";
-            tipo = "";
+            case 1:
+                tipo = "Comum";
+                break;
+            case 2:
+                tipo = "Prioritario";
+                break;
+            case 3:
+                tipo = "Emergencia";
+                break;
+            case 4:
+                tipo = "Servico";
+                break;
+            default:
+                cout << "Opçao invalida. Tipo definido como 'Comum'.\n";
+                tipo = "Comum";
         }
-        ~Veiculo() 
-        {
-        }
-        void set_dados()
-        {
-            cout << "Qual a placa do veiculo? ";
-            getline(cin>>ws, placa);
-            cout << "Qual o modelo do veiculo? ";
-            getline(cin>>ws, modelo);
-            cout << "Qual o tipo do veiculo? (Comum, Prioritario ou Emergencia) ";
-            getline(cin>>ws, tipo);
-        }
-        void print_dados()
-        {
-            cout << "Placa: " << placa << endl;
-            cout << "Modelo: " << modelo << endl;
-            cout << "Tipo: " << tipo << endl;
-        }
-        string get_placa()
-        {
-            return placa;
-        }
-        string get_modelo()
-        {
-            return modelo;
-        }
-        string get_tipo()
-        {
-            return tipo;
-        }
-    };
+    }
+
+    void print_dados() const
+    {
+        cout << "Placa: " << placa << endl;
+        cout << "Tipo: " << tipo << endl;
+    }
+
+    string get_placa() const
+    {
+        return placa;
+    }
+    
+    string get_tipo() const
+    {
+        return tipo;
+    }
+
+    void set_placa(const string &placa_veiculo)
+    {
+        placa = placa_veiculo;
+    }
+
+    void set_tipo(const string &tipo_veiculo)
+    {
+        tipo = tipo_veiculo;
+    }
+};
 
 #endif // VEICULO_H
